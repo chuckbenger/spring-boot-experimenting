99881a9532ae086bbbe89273b4a85639bb58fc40
chuckbenger <chuckbenger@gmail.com>
2023-05-23 20:14:46 -0400

    Cleaning up Tweet Controller Paths

--- src/main/kotlin/com/example/springbootexperimenting/controllers/TweetController.kt
+++ src/main/kotlin/com/example/springbootexperimenting/controllers/TweetController.kt
@@ -12,19 +12,19 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/tweets")
 class TweetController(private val tweetService: TweetService) {
 
-    @GetMapping("/")
+    @GetMapping
     fun getTweets(): ResponseEntity<List<TweetResponse>> {
         val tweets = tweetService.getTweets()
         return ResponseEntity.ok(tweets)
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     fun getTweet(@PathVariable id: Long): ResponseEntity<TweetResponse> {
         val tweet = tweetService.getTweet(id)
         return ResponseEntity.ok(tweet)
     }
 
-    @PostMapping("/")
+    @PostMapping
     fun createTweet(@Valid @RequestBody tweetRequest: TweetRequest): ResponseEntity<TweetResponse> {
         val tweet = tweetService.createTweet(tweetRequest)
         return ResponseEntity.status(HttpStatus.CREATED).body(tweet)
